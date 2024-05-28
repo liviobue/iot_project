e484353827026ecce6feda2a200547ff61163160
danielakomenda <d.komenda@hotmail.com>
2024-05-28 13:22:19 +0200

    Monkey-Patch Picarx

--- src/iot_project/sunfounder_picar/picarx_control.py
+++ src/iot_project/sunfounder_picar/picarx_control.py
@@ -23,8 +23,8 @@ control_history = []
 
 async def car_control_loop(real_picar: bool = True):
     if real_picar:
-        import picarx
-        picarx.os = DummyOSModule()
+        import picarx.picarx
+        picarx.picarx.os = DummyOSModule()
         print("real picar")
         px = picarx.Picarx()
 
